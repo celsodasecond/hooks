3b3515b7ac8197faa91a96d03a812d9436176f98
celsodasecond <75917932+celsodasecond@users.noreply.github.com>
2023-08-17 18:27:10 +0800

    init

---
+++ src/components/TodoList.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const TodoList = () => {
+	return (
+		<div>
+			<ul>
+				<li>Pay Bills</li>
+				<li>Do Your Homework</li>
+				<li>Feed the Dog</li>
+			</ul>
+		</div>
+	);
+};
+
+export default TodoList;
